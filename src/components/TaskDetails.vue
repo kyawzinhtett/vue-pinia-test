4b0d0e2ee8c59b28c1da62509f5650c93d14052f
kyawzinhtet <itskyawzinhtet@gmail.com>
2023-10-09 23:02:29 +0630

    feat: add color to fav icon

--- src/components/TaskDetails.vue
+++ src/components/TaskDetails.vue
@@ -3,7 +3,10 @@
         <h3>{{ task.title }}</h3>
         <div class="icons">
             <i class="material-symbols-outlined" @click="taskStore.deleteTask(task.id)">delete</i>
-            <i class="material-symbols-outlined" @click="taskStore.toggleTask(task.id)">favorite</i>
+            <i class="material-symbols-outlined" :style="{ color: task.isFav ? '#ff005d' : '' }"
+                @click="taskStore.toggleTask(task.id)">
+                favorite
+            </i>
         </div>
     </div>
 </template>
